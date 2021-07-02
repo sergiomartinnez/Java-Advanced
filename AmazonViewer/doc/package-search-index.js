c46fdf6c0de7567a5cb1c0879a9b9481b54b66ef
sergio lozano <conomongo@hotmail.com>
2021-07-01 20:28:15 -0500

    amazon documents v2

---
+++ AmazonViewer/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.anncode.amazonviewer"},{"l":"com.anncode.amazonviewer.model"},{"l":"com.anncode.util"}];updateSearchResults();
